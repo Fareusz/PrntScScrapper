cd4f82bd08791656013c11184484119785e4e2ce
Fareusz <fariares809@gmail.com>
2021-12-03 09:35:12 +0100

    Code cleaned by @GilbertZRivii

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
-import random
-import string
-import webbrowser
+import random, string, webbrowser, cloudscraper, requests, urllib.request, os
 from bs4 import BeautifulSoup
-import cloudscraper
-from tkinter import *
 from tkinter import messagebox
-import requests
-import urllib.request
-import os
+import tkinter as tk
 from datetime import datetime
-
 
 # The creator is not responsible for the screenshots found by this script!!!
 
 new = 1
 
-root = Tk()
+root = tk.Tk()
 root.title("PrntScScapper by Fareusz")
 root.geometry("500x250")
+
 
 def openweb(link):
     webbrowser.open(link, new=new)
@@ -28,11 +22,17 @@ def rename():
     now = now.replace(":", "-")
     os.rename('image.jpg', f"{now}.jpg")
 
-scraper = cloudscraper.create_scraper()
-resultList = list()
+
+def on_closing():
+    if messagebox.askokcancel("Quit", "Do you want to quit?"):
+        try:
+            os.remove("image.jpg")
+            root.destroy()
+        except:
+            root.destroy()
+
 
 def FindLinkPrnt():
-    print(brwser)
     found = 0
     while found == 0:
         id = ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
@@ -54,23 +54,30 @@ def FindLinkPrnt():
         if 'https://i.imgur.com/removed.png' in source:
             print('Unknown link.. skipping...' + str(id))
             continue
+
         res = requests.get(results['src'])
         response = res.status_code
+
         if response == 200:
             response = '✓  Success!'
-            found =+ 1
-        if response == 520:
+            found = + 1
+
+        elif response == 520:
             response = '✓  Success!'
-            found =+ 1
-        if response == 404:
+            found = + 1
+
+        elif response == 404:
             response = 'X  Not Found'
             continue
-        if response == 403:
+
+        elif response == 403:
             response = 'X Forbidden'
             continue
+
         b = brwser.get()
         if b == 1:
             openweb(source)
+
         opener = urllib.request.build_opener()
         opener.addheaders = [('User-Agent',
                               'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
@@ -78,38 +85,33 @@ def FindLinkPrnt():
         filename = 'image.jpg'
         image_url = source
         urllib.request.urlretrieve(image_url, filename)
-        root2 = Tk()
+        root2 = tk.Tk()
+
         def next():
             root2.destroy()
             FindLinkPrnt()
-        img = PhotoImage(master=root2, file="image.jpg")
-        button3 = Button(root2, width=2, height=2, text = 'Next!', font = 'ComicSans', bg='black', fg='green', padx = 10, command = next)
-        button2 = Button(root2, width=2, height=2, text = 'Save!', font = 'ComicSans', bg='black', fg='green', padx = 10, command = rename)
+
+        img = tk.PhotoImage(master=root2, file="image.jpg")
+        button3 = tk.Button(root2, width=2, height=2, text='Next!', font='ComicSans', bg='black', fg='green', padx=10,
+                            command=next)
+        button2 = tk.Button(root2, width=2, height=2, text='Save!', font='ComicSans', bg='black', fg='green', padx=10,
+                            command=rename)
         button2.pack()
         button3.pack()
-        label = Label(root2, image=img)
+        label = tk.Label(root2, image=img)
         label.pack()
         root2.mainloop()
 
 
+scraper = cloudscraper.create_scraper()
+resultList = list()
 
-Button(root, width=50, height=10, text = 'Get Link', font = 'ComicSans', bg='black', fg='green', padx = 10, command = FindLinkPrnt).place(x=10,y=10)
+tk.Button(root, width=50, height=10, text='Get Link', font='ComicSans', bg='black', fg='green', padx=10,
+          command=FindLinkPrnt).place(x=10, y=10)
 
-brwser = IntVar()
-c = Checkbutton(root, text="Open in Browser at hit", variable=brwser, onvalue=1, offvalue=0)
-c.pack()
-c.place(x=200,y=205)
-
-def on_closing():
-    if messagebox.askokcancel("Quit", "Do you want to quit?"):
-        try:
-            os.remove("image.jpg")
-            root.destroy()
-        except:
-            root.destroy()
-
+brwser = tk.IntVar()
+c = tk.Checkbutton(root, text="Open in Browser at hit", variable=brwser, onvalue=1, offvalue=0)
+c.place(x=200, y=205)
 
 root.protocol("WM_DELETE_WINDOW", on_closing)
-
-
 root.mainloop()
